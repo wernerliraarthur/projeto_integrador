fec5fbb615132276b5f46c831532ab41685d7261
Arthur Werner Lira <105883063+wernerliraarthur@users.noreply.github.com>
2022-09-30 15:39:16 -0300

    animação JavaScript

--- javaScript/myScript.js
+++ javaScript/myScript.js
@@ -1,1 +1,19 @@
+var number = 1;
 
+function trocarFrase() {
+    number = number + 1;
+    var fraseAtual = document.getElementById("frase");
+    fraseAtual.style.transition = "2s";
+    fraseAtual.style.marginLeft = "70px";
+    fraseAtual.style.color = "red";
+    if (number % 2 == 0) {
+        var fraseAtual = document.getElementById("frase");
+        fraseAtual.innerHTML = "Voltar a página principal";
+    } else {
+        var fraseAtual = document.getElementById("frase");
+        fraseAtual.innerHTML = "Computadores na Segunda Guerra";
+        fraseAtual.style.transition = "2s";
+        fraseAtual.style.marginLeft = "0px";
+        fraseAtual.style.color = "white";
+    }
+}
